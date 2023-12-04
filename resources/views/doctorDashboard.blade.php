1773921304e3af6e654feaf71e7af153531e4cd2
CardCastle8919 <jonathancoltonk@gmail.com>
2023-12-04 15:07:23 -0500

    jon did a lot of error fixes to the styling of pages

--- resources/views/doctorDashboard.blade.php
+++ resources/views/doctorDashboard.blade.php
@@ -5,48 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Doctor Dashboard</title>
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Updateddashboard.css') }}">
 </head>
 <body>
     <div class="scene">
         <div class="sun"></div>
         <div class="water"></div>
-<div class="hold3">
-    <h1 class="boxed">Doctor Dashboard</h1>
-</div>
-    
-    {{-- <br>
-    <br>
-    <h2>You are signed in as</h2>
-    <h2>firstName</h2>
-    <h2>lastname</h2> --}}
-    <div class="hold2">
-        <div class="hold">
-            <form action={{ url('/doctorsHome') }} method="get">
-                <button id="btn1">Doctors Home</button>
-            </form>
-            <br>
-            <br>
-            <form action={{ url('/patientOfDoc') }} method="get">
-                <button id="btn1">Patient of Doctor</button>
-            </form>
-        </div>
-        <div class="hold">
-            <form action={{ url('/patients') }} method="get">
-                <button id="btn1">Patients</button>
-            </form>
-            <br>
-            <br>
-            <form action={{ url('/rosterNewRoster') }} method="get">
-                <button id="btn1">Roster</button>
-            </form>
-        </div>
-    
-   
-    <br>
-    <br>
     </div>
-    
-</div>
+    <header class="nohead">
+        <div>Sunrise name place holder</div>
+          <h1 class="boxed">Doctor Dashboard</h1>
+            <div class="user-dropdown">
+                <button id="btn2">Profile</button>
+            <div class="dropdown-content">
+                <a href="#">First Name</a>
+                <a href="#">Last Name</a>
+                <a href="#">Logout</a>
+            </div>
+        </div>
+    </header>
+    <div id="container">
+        <div class="banner" >
+            {{-- just ignore this max I know youll probably see this but please this random div its actually important --}}
+        </div>
+            
+            {{-- <br>
+            <br>
+            <h2>You are signed in as</h2>
+            <h2>firstName</h2>
+            <h2>lastname</h2> --}}
+        <div id="hold">
+            <div class="btn-con">
+                <form action={{ url('/doctorsHome') }} method="get">
+                    <button class="btn1">Doctors Home</button>
+                </form>
+                <form action={{ url('/patientOfDoc') }} method="get">
+                    <button class="btn1">Patient of Doctor</button>
+                </form>
+            </div>
+            <div class="btn-con">
+                <form action={{ url('/patients') }} method="get">
+                    <button class="btn1">Patients</button>
+                </form>
+                <form action={{ url('/rosterNewRoster') }} method="get">
+                    <button class="btn1">Roster</button>
+                </form>
+            </div>
+        </div> 
+    </div>
 </body>
 </html>
