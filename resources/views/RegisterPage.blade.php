53c7bf62a48083785f5cbc2211c54eccc2d07dee
Renzolution <renzolutionn@gmail.com>
2023-12-07 13:27:02 -0500

    lorenzo Add header to all Admin Pages
    Headers added to all admin pages

--- resources/views/RegisterPage.blade.php
+++ resources/views/RegisterPage.blade.php
@@ -12,125 +12,132 @@
     <title>Register Page</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-  const starsContainer = document.getElementById("stars");
+        document.addEventListener("DOMContentLoaded", function() {
+            const starsContainer = document.getElementById("stars");
 
-  // Function to generate a random number within a range
-  function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+            // Function to generate a random number within a range
+            function getRandomNumber(min, max) {
+                return Math.random() * (max - min) + min;
+            }
 
-  // Function to generate stars and add them to the starsContainer
-  function generateStars() {
-    for (let i = 0; i < 300; i++) {
-      const star = document.createElement("div");
-      star.className = "star";
-      star.style.top = `${getRandomNumber(0, 100)}%`;
-      star.style.left = `${getRandomNumber(0, 100)}%`;
+            // Function to generate stars and add them to the starsContainer
+            function generateStars() {
+                for (let i = 0; i < 300; i++) {
+                    const star = document.createElement("div");
+                    star.className = "star";
+                    star.style.top = `${getRandomNumber(0, 100)}%`;
+                    star.style.left = `${getRandomNumber(0, 100)}%`;
 
-      // Set a random delay for each star
-      const delay = getRandomNumber(0, 5); // Adjust the range as needed
-      star.style.animationDelay = `-${delay}s`;
+                    // Set a random delay for each star
+                    const delay = getRandomNumber(0, 5); // Adjust the range as needed
+                    star.style.animationDelay = `-${delay}s`;
 
-      starsContainer.appendChild(star);
-    }
-  }
-
-  generateStars();
-});
-        $(function (){
-            $("#role").change(function (){
-                if(this.value == "Patient"){
-                    $("#Patient-Fields").slideDown("slow");
+                    starsContainer.appendChild(star);
                 }
-                else{
-                    $("#Patient-Fields").slideUp("slow"); 
+            }
+
+            generateStars();
+        });
+        $(function() {
+            $("#role").change(function() {
+                if (this.value == "Patient") {
+                    $("#Patient-Fields").slideDown("slow");
+                } else {
+                    $("#Patient-Fields").slideUp("slow");
                 }
             });
         });
-
     </script>
     <style>
-        #Patient-Fields{
+        #Patient-Fields {
             display: none;
         }
-        #btn1{
-      width: 100%;
-      height: 70px;
-      border: 2px solid black;
-      border-radius: 5px;
-      background-color: transparent;
-      color: white; 
-            font-size: 25px; 
-            text-shadow: -2px 2px 0 #000, 
-                          2px 2px 0 #000, 
-                         2px -2px 0 #000, 
-                        -2px -2px 0 #000;
-      text-shadow: h-shadow v-shadow blur-radius color;
-    }
-    #btn1:hover{
-    background-color:#0d47a1
-    }
-    .hold{
-        margin-top: 5px;
-      border-color: 1px solid black;
-      padding: 70px;
-      border-radius: 10px;
-      background-color: rgba(0, 0, 0, .2);
-      border: #000 2px solid;
-      box-shadow: 5px 5px 10px black;
-      color: white;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 800px;
-      flex-direction: column;
-      width: 400px;
-      margin-bottom: 30px;
-    }
-    h1{
-      font-family: 'DM Serif Display', serif;
-      text-shadow: h-shadow v-shadow blur-radius color;
-      color: white; 
-            font-size: 35px; 
-            text-shadow: -1px 1px 0 #000, 
-                          1px 1px 0 #000, 
-                         1px -1px 0 #000, 
-                        -1px -1px 0 #000;
-    }
-    .lg{
-        width: 100%;
 
-    }
-    .lg2{
-        width: 100%;
-        height: 27px;
-        border-radius: 5px;
-    }
-    .marg{
-        margin-bottom: 10px;
-    }
-    #btn2{
-      width: 100%;
-      height: 70px;
-      border: 2px solid black;
-      border-radius: 5px;
-      background-color: transparent;
-      color: white; 
-            font-size: 25px; 
-            text-shadow: -2px 2px 0 #000, 
-                          2px 2px 0 #000, 
-                         2px -2px 0 #000, 
-                        -2px -2px 0 #000;
-      text-shadow: h-shadow v-shadow blur-radius color;
-      margin-bottom: 20px;
-      padding-left: 90px;
-      padding-right: 90px;
-      margin-bottom: 20px;
-    }
-    #btn2:hover{
-    background-color:#0d47a1
-    }
+        #btn1 {
+            width: 100%;
+            height: 70px;
+            border: 2px solid black;
+            border-radius: 5px;
+            background-color: transparent;
+            color: white;
+            font-size: 25px;
+            text-shadow: -2px 2px 0 #000,
+                2px 2px 0 #000,
+                2px -2px 0 #000,
+                -2px -2px 0 #000;
+            text-shadow: h-shadow v-shadow blur-radius color;
+        }
+
+        #btn1:hover {
+            background-color: #0d47a1
+        }
+
+        .hold {
+            margin-top: 5px;
+            border-color: 1px solid black;
+            padding: 70px;
+            border-radius: 10px;
+            background-color: rgba(0, 0, 0, .2);
+            border: #000 2px solid;
+            box-shadow: 5px 5px 10px black;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 800px;
+            flex-direction: column;
+            width: 400px;
+            margin-bottom: 30px;
+        }
+
+        h1 {
+            font-family: 'DM Serif Display', serif;
+            text-shadow: h-shadow v-shadow blur-radius color;
+            color: white;
+            font-size: 35px;
+            text-shadow: -1px 1px 0 #000,
+                1px 1px 0 #000,
+                1px -1px 0 #000,
+                -1px -1px 0 #000;
+        }
+
+        .lg {
+            width: 100%;
+
+        }
+
+        .lg2 {
+            width: 100%;
+            height: 27px;
+            border-radius: 5px;
+        }
+
+        .marg {
+            margin-bottom: 10px;
+        }
+
+        #btn2 {
+            width: 100%;
+            height: 70px;
+            border: 2px solid black;
+            border-radius: 5px;
+            background-color: transparent;
+            color: white;
+            font-size: 25px;
+            text-shadow: -2px 2px 0 #000,
+                2px 2px 0 #000,
+                2px -2px 0 #000,
+                -2px -2px 0 #000;
+            text-shadow: h-shadow v-shadow blur-radius color;
+            margin-bottom: 20px;
+            padding-left: 90px;
+            padding-right: 90px;
+            margin-bottom: 20px;
+        }
+
+        #btn2:hover {
+            background-color: #0d47a1
+        }
     </style>
 </head>
 
@@ -141,91 +148,92 @@
         <div class="stars" id="stars"></div>
     </div>
     <div id="container">
-       
-        <div class="hold"> 
+
+        <div class="hold">
             <h1>Registration</h1>
             <form id="registrationForm" action="addToRegister" method="POST" class="lg">
-            @csrf
-            <div class="marg">
-                <label for="Role_ID" id="font1">Role:</label>
-                <select id="role" name="Role_ID" class="lg2" required>
-                    <option value="Admin">Admin</option>
-                    <option value="Supervisor">Supervisor</option>
-                    <option value="Doctor">Doctor</option>
-                    <option value="Caregiver">Caregiver</option>
-                    <option value="Patient">Patient</option>
-                    <!-- Add other roles as needed -->
-                </select>
-            </div>
-            <div class="marg">
-                <label for="First_Name" id="font1">First Name:</label>
-                <br>
-                <input type="text" id="First_Name" name="First_Name" required class="lg2">
-            </div>
-            <div class="marg">
-                <label for="Last_Name" id="font1">Last Name:</label>
-                <br>
-                <input type="text" id="Last_Name" name="Last_Name" required class="lg2">
-            </div>
-            <div class="marg">
-                <label for="Email" id="font1">Email:</label>
-                <br>
-                <input type="Email" id="Email" name="Email" required class="lg2">
-            </div>
-            <div class="marg">
-                <label for="Phone" id="font1">Phone:</label>
-                <br>
-                <input type="tel" id="Phone" name="Phone" required class="lg2">
-            </div>
-            <div class="marg">
-                <label for="Password" id="font1">Password:</label>
-                <br>
-                <input type="Password" id="Password" name="Password" required class="lg2">
-            </div>
-            <div class="marg">
-                <label for="dob" id="font1">Date of Birth:</label>
-                <br>
-                <input type="date" id="dob" name="dob" required class="lg2">
-            </div>
-            <div id="Patient-Fields">
+                @csrf
                 <div class="marg">
-                    <label for="Family_Code" id="font1">Family Code (this will be used by your family members):</label>
-                    <br>
-                    <input type="number" id="family-code" name="Family_Code" value={{ $FamilyCode }} readonly>
+                    <label for="Role_ID" id="font1">Role:</label>
+                    <select id="role" name="Role_ID" class="lg2" required>
+                        <option value="Admin">Admin</option>
+                        <option value="Supervisor">Supervisor</option>
+                        <option value="Doctor">Doctor</option>
+                        <option value="Caregiver">Caregiver</option>
+                        <option value="Patient">Patient</option>
+                        <!-- Add other roles as needed -->
+                    </select>
                 </div>
                 <div class="marg">
-                    <label for="Emergency_Contact" id="font1">Emergency Contact Name:</label>                
+                    <label for="First_Name" id="font1">First Name:</label>
                     <br>
-                    <input type="text" id="emergency-contact" name="Emergency_Contact" class="lg2">
+                    <input type="text" id="First_Name" name="First_Name" required class="lg2">
                 </div>
-                <div class="marg" >
-                    <label for="Contact_Relation" id="font1">Relation to Contact:</label>
+                <div class="marg">
+                    <label for="Last_Name" id="font1">Last Name:</label>
                     <br>
-                    <input type="text" id="emergency-contact-relation" name="Contact_Relation" class="lg2">
+                    <input type="text" id="Last_Name" name="Last_Name" required class="lg2">
                 </div>
-            </div>
-            <br>
-            <!-- Add more fields as needed -->
-            <div class="lg" >
-                <button type="submit" value="Register" id="btn1">Register</button>
-            </div>
-        </form>
-        <div class="cancelAlert">
-            <button onclick="showAlert()">Cancel</button>
-            
-            <div id="overlay" onclick="hideAlert()"></div>
-            <div id="alertBox">
-            <p>Do you want to reset the form?</p>
-            <button onclick="resetForm()">Reset</button>
-            <button onclick="hideAlert()">Cancel</button>
-            </div>
-        </div>
-        <div class="form-group">
-            <form action={{ url('/welcome') }} method="get">
-                <button>Return to Home Page</button>
+                <div class="marg">
+                    <label for="Email" id="font1">Email:</label>
+                    <br>
+                    <input type="Email" id="Email" name="Email" required class="lg2">
+                </div>
+                <div class="marg">
+                    <label for="Phone" id="font1">Phone:</label>
+                    <br>
+                    <input type="tel" id="Phone" name="Phone" required class="lg2">
+                </div>
+                <div class="marg">
+                    <label for="Password" id="font1">Password:</label>
+                    <br>
+                    <input type="Password" id="Password" name="Password" required class="lg2">
+                </div>
+                <div class="marg">
+                    <label for="dob" id="font1">Date of Birth:</label>
+                    <br>
+                    <input type="date" id="dob" name="dob" required class="lg2">
+                </div>
+                <div id="Patient-Fields">
+                    <div class="marg">
+                        <label for="Family_Code" id="font1">Family Code (this will be used by your family
+                            members):</label>
+                        <br>
+                        <input type="number" id="family-code" name="Family_Code" value={{ $FamilyCode }} readonly>
+                    </div>
+                    <div class="marg">
+                        <label for="Emergency_Contact" id="font1">Emergency Contact Name:</label>
+                        <br>
+                        <input type="text" id="emergency-contact" name="Emergency_Contact" class="lg2">
+                    </div>
+                    <div class="marg">
+                        <label for="Contact_Relation" id="font1">Relation to Contact:</label>
+                        <br>
+                        <input type="text" id="emergency-contact-relation" name="Contact_Relation" class="lg2">
+                    </div>
+                </div>
+                <br>
+                <!-- Add more fields as needed -->
+                <div class="lg">
+                    <button type="submit" value="Register" id="btn1">Register</button>
+                </div>
             </form>
-        </div>
-        
+            <div class="cancelAlert">
+                <button onclick="showAlert()">Cancel</button>
+
+                <div id="overlay" onclick="hideAlert()"></div>
+                <div id="alertBox">
+                    <p>Do you want to reset the form?</p>
+                    <button onclick="resetForm()">Reset</button>
+                    <button onclick="hideAlert()">Cancel</button>
+                </div>
+            </div>
+            <div class="form-group">
+                <form action={{ url('/welcome') }} method="get">
+                    <button>Return to Home Page</button>
+                </form>
+            </div>
+
         </div>
     </div>
 </body>
--- resources/views/additionalPatientInfo.blade.php
+++ resources/views/additionalPatientInfo.blade.php
@@ -1,92 +1,144 @@
 <!DOCTYPE html>
 <html lang='en'>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://unpkg.com/bamboo.css/dist/dark.min.css">
-        <!--Style for form alerts-->
-        <link rel="stylesheet" href="css/style.css" type="text/css">
-        <!--Functions for form alerts-->
-        <script src="app.js"></script>
-        <title>Additional Patient Information</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script>
-            var patients = <?php echo $patients;?>;
-            $(function(){
-                $("#Patient-ID").change(function(){
-                    var patientExists = false;
-                    for (const patient of patients){
-                        if(this.value == patient[Object.keys(patient)[0]]){
-                            $("#First-Name").val(patient[Object.keys(patient)[1]]).fadeIn('slow');
-                            $("#Patient-Group").val(patient[Object.keys(patient)[8]]);
-                            $("#Addmission-Date").val(patient[Object.keys(patient)[9]]);
-                            $("#First-Name").slideDown("slow");
-                            $(".New-Patient-Group").slideDown("slow");
-                            patientExists = true;
-                            break;
-                        }
-                        else{
-                            patientExists = false;
-                        }
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/bamboo.css/dist/dark.min.css">
+
+    <!--Style for form alerts-->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <!--Functions for form alerts-->
+    <script src="app.js"></script>
+    <title>Additional Patient Information</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        var patients = <?php echo $patients; ?>;
+        $(function() {
+            $("#Patient-ID").change(function() {
+                var patientExists = false;
+                for (const patient of patients) {
+                    if (this.value == patient[Object.keys(patient)[0]]) {
+                        $("#First-Name").val(patient[Object.keys(patient)[1]]).fadeIn('slow');
+                        $("#Patient-Group").val(patient[Object.keys(patient)[8]]);
+                        $("#Addmission-Date").val(patient[Object.keys(patient)[9]]);
+                        $("#First-Name").slideDown("slow");
+                        $(".New-Patient-Group").slideDown("slow");
+                        patientExists = true;
+                        break;
+                    } else {
+                        patientExists = false;
                     }
-                    if(!patientExists){
-                        $("#First-Name").slideUp("slow");
-                            $(".New-Patient-Group").slideUp("slow");
-                            $("#First-Name").val(null);
-                            $("#Patient-Group").val(null);
-                            $("#Addmission-Date").val(null);
-                    }
-                });
-                $("#reset").click(function(){
+                }
+                if (!patientExists) {
                     $("#First-Name").slideUp("slow");
-                    $("#New-Patient-Group").slideUp("slow");
-                    $("#New-Patient-Group-Input").slideUp("slow");
-                });
+                    $(".New-Patient-Group").slideUp("slow");
+                    $("#First-Name").val(null);
+                    $("#Patient-Group").val(null);
+                    $("#Addmission-Date").val(null);
+                }
             });
-        </script>
-        <style>
-            #First-Name{
-                display: none;
-            }
-            .New-Patient-Group{           
-                display: none;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Addtional Patient Information</h1>
+            $("#reset").click(function() {
+                $("#First-Name").slideUp("slow");
+                $("#New-Patient-Group").slideUp("slow");
+                $("#New-Patient-Group-Input").slideUp("slow");
+            });
+        });
+    </script>
+    <style>
+        #First-Name {
+            display: none;
+        }
+
+        .New-Patient-Group {
+            display: none;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 15px;
+            text-align: center;
+        }
+
+        .user-dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 120px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
+
+        .user-dropdown:hover .dropdown-content {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>Addtional Patient Information Dashboard</h1>
+        <div class="user-dropdown">
+            <button id="btn2">Profile</button>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Logout</a>
+            </div>
+        </div>
+    </header>
+    <br>
+    <br>
+    <form id="form" action="changePatientGroup" method="POST">
+        @csrf
+        Enter a Patient's ID:
+        <input type="number" name="Patient_ID" id="Patient-ID">
+        <input style="margin-top: 5px" type="text" name="First_Name" id="First-Name" readonly>
         <br>
         <br>
-        <form id="form" action="changePatientGroup" method="POST">
-            @csrf
-            Enter a Patient's ID:
-            <input type="number" name="Patient_ID" id="Patient-ID">
-            <input style="margin-top: 5px" type="text" name="First_Name" id="First-Name" readonly>
-            <br>
-            <br>
-            Patient's Current Group:
-            <input type="number" name="Patient_Group" id="Patient-Group" readonly>
-            <br>
-            <br>
-            Patient's Admission Date:
-            <input type="date" name="Addmission_Date" id="Addmission-Date" readonly>
-            <br>
-            <label style="margin-top: 15px" id="New-Patient-Group" for="New-Patient-Group" class="New-Patient-Group">Enter a New Group for Patient:</label>
-            <input style="margin-bottom: 15px" id="New-Patient-Group-Input" type="number" class="New-Patient-Group" name="New-Patient-Group" min="1" max="4" required>
-            <br>
-            <button type="Submit">Change Patient's Group</button>
-        </form>
+        Patient's Current Group:
+        <input type="number" name="Patient_Group" id="Patient-Group" readonly>
         <br>
-        <div class="cancelAlert">
-            <button onclick="showAlert()">Cancel</button>
-            
-            <div id="overlay" onclick="hideAlert()"></div>
-            <div id="alertBox">
+        <br>
+        Patient's Admission Date:
+        <input type="date" name="Addmission_Date" id="Addmission-Date" readonly>
+        <br>
+        <label style="margin-top: 15px" id="New-Patient-Group" for="New-Patient-Group" class="New-Patient-Group">Enter a
+            New Group for Patient:</label>
+        <input style="margin-bottom: 15px" id="New-Patient-Group-Input" type="number" class="New-Patient-Group"
+            name="New-Patient-Group" min="1" max="4" required>
+        <br>
+        <button type="Submit">Change Patient's Group</button>
+    </form>
+    <br>
+    <div class="cancelAlert">
+        <button onclick="showAlert()">Cancel</button>
+
+        <div id="overlay" onclick="hideAlert()"></div>
+        <div id="alertBox">
             <p>Do you want to reset the form?</p>
             <button id="reset" onclick="resetForm()">Reset</button>
             <button onclick="hideAlert()">Cancel</button>
-            </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
