6d59f09ce8fc0c36e3e715752075ff032149881d
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-12-11 15:17:51 -0500

    Ability to create a new roster, and connect the correct caregiver to the assigned group

--- resources/views/additionalPatientInfo.blade.php
+++ resources/views/additionalPatientInfo.blade.php
@@ -9,13 +9,21 @@
         <!--Style for form alerts-->
         <link rel="stylesheet" href="css/style.css" type="text/css">
         <!--Functions for form alerts-->
-        <script src="app.js"></script>
         <title>Additional Patient Information</title>
+        <style>
+            #First-Name{
+                display: none;
+            }
+        </style>
+        <script src="app.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script>
             var patients = <?php echo $patients;?>;
-            $(function(){
+            console.log(patients);
+
+            $(document).ready(function(){
                 $("#Patient-ID").change(function(){
+                    console.log('true');
                     var patientExists = false;
                     for (const patient of patients){
                         if(this.value == patient[Object.keys(patient)[0]]){
@@ -30,53 +38,55 @@
                         else{
                             patientExists = false;
                         }
-                }
-                if (!patientExists) {
+                    }
+                    if (!patientExists) {
+                        $("#First-Name").slideUp("slow");
+                        $(".New-Patient-Group").slideUp("slow");
+                        $("#First-Name").val(null);
+                        $("#Patient-Group").val(null);
+                        $("#Addmission-Date").val(null);
+                    }
+                });
+
+                $("#reset").click(function() {
                     $("#First-Name").slideUp("slow");
-                    $(".New-Patient-Group").slideUp("slow");
-                    $("#First-Name").val(null);
-                    $("#Patient-Group").val(null);
-                    $("#Addmission-Date").val(null);
-                }
-            });
-            $("#reset").click(function() {
-                $("#First-Name").slideUp("slow");
-                $("#New-Patient-Group").slideUp("slow");
-                $("#New-Patient-Group-Input").slideUp("slow");
+                    $("#New-Patient-Group").slideUp("slow");
+                    $("#New-Patient-Group-Input").slideUp("slow");
+                });
             });
         </script>
     </head>
-    <header>
-        <h3>
-            Sunrise Retirement Home
-        </h3>
-        <div class='header-btn-section'>
-            <form action="back" method='POST'>
-                @csrf
-                <button type='Submit'>Back</button>
-            </form>
-        </div>
-        <div class='header-btn-section'>
-            <div class="user-dropdown">
-                <button id="btn2">Profile</button>
-                <div class="dropdown-content">
-                    {{-- <a href="#">{{ $First_Name }}</a> --}}
-                    {{-- <a href="#">{{ $Last_Name }}</a> --}}
-                    You exist!
-                </div>
-            </div>
-        </div>
-        <div class='header-btn-section'>
-            <form action="logout" method='POST'>
-                @csrf
-                <button type='Submit'>Logout</button>
-            </form>
-        </div>
-    </header>
     <body>
         <div class="scene">
             <div class="sun"></div>
         </div>
+        <header>
+            <h3>
+                Sunrise Retirement Home
+            </h3>
+            <div class='header-btn-section'>
+                <form action="back" method='POST'>
+                    @csrf
+                    <button type='Submit'>Back</button>
+                </form>
+            </div>
+            <div class='header-btn-section'>
+                <div class="user-dropdown">
+                    <button id="btn2">Profile</button>
+                    <div class="dropdown-content">
+                        {{-- <a href="#">{{ $First_Name }}</a> --}}
+                        {{-- <a href="#">{{ $Last_Name }}</a> --}}
+                        You exist!
+                    </div>
+                </div>
+            </div>
+            <div class='header-btn-section'>
+                <form action="logout" method='POST'>
+                    @csrf
+                    <button type='Submit'>Logout</button>
+                </form>
+            </div>
+        </header>
           <div id="container">
             <div class="banner">
             {{-- just ignore this max I know youll probably see this but please this random div its actually important --}}
@@ -88,16 +98,16 @@
                 <form id="form" action="changePatientGroup" method="POST">
                     @csrf
                     Enter a Patients ID:
-                    <input id="textbox" type="number" name="Patient_ID" id="Patient-ID">
+                    <input class="textbox" type="number" name="Patient_ID" id="Patient-ID">
                     <input type="text" name="First_Name" id="First-Name" readonly>
                     <br>
                     <br>
                     Current Group:
-                    <input id="textbox" type="number" name="Patient_Group" id="Patient-Group" readonly>
+                    <input class="textbox" type="number" name="Patient_Group" id="Patient-Group" readonly>
                     <br>
                     <br>
                     Admission Date:
-                    <input id="textbox" type="date" name="Addmission_Date" id="Addmission-Date" readonly>
+                    <input class="textbox" type="date" name="Addmission_Date" id="Addmission-Date" readonly>
                     <br>
                     <label for="New-Patient-Group" class="New-Patient-Group">New Group For Patient:</label>
                     <input type="number" class="New-Patient-Group" name="New-Patient-Group" min="1" max="4" required>
